72774647b91a5bbc2edae434327ceb661f40ebe8
AlfredPietraru <alfred.pietraru@gmail.com>
2024-09-13 19:05:08 +0300

    add transforms

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import torch
-import torchvision.transforms.functional
+from torchvision.transforms.functional import rotate
 import preprocessing as prep
 from net import AutoEncoder
 from torch.utils.data import  DataLoader
-import torchvision
 import os
 LEARNING_RATE = 1e-3
-EPOCHS = 1
+EPOCHS = 3
 
 class ImageDataset(torch.utils.data.Dataset):
     def __init__(self):
@@ -15,26 +14,36 @@ class ImageDataset(torch.utils.data.Dataset):
     def __len__(self):
         elements  = os.listdir(self.root_dir)
         return len(elements)
-
+  
     def __getitem__(self, idx):
         if (idx < 0 | idx > self.__len__()):
             return None
         return prep.get_full_training_image(idx)
 
+def transformers(img : torch.tensor):
+  return torch.cat([rotate(img, angle=90),
+      rotate(img, angle=180), rotate(img, angle=270)], dim=0)
+
+
 def training_loop(dataloader : DataLoader):
     model = AutoEncoder()
-    model.load_state_dict(torch.load("./reconstruction.pth"))
+    if (os.path.isfile("./reconstruction.pth")):
+      model.load_state_dict(torch.load("./reconstruction.pth"))
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
     loss_fn = torch.nn.MSELoss()
     for i in range(EPOCHS):
       for _, img_info in  enumerate(dataloader):
         img, modified, _ = img_info
-        loss = loss_fn(model(modified), img)
-        print(loss)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        for i in range(0, 4):
+          if (i == 0):
+             loss =loss_fn(model(modified), img)
+          if (i != 0):
+            loss = loss_fn(model(rotate(modified, i * 90)), rotate(img, i * 90))
+          optimizer.zero_grad()
+          print(loss)
+          loss.backward()
+          optimizer.step()
       print("gata epoca %d", i)
       torch.save(model.state_dict(), "./reconstruction.pth")
         
@@ -43,12 +52,13 @@ def eval_loop(idx : int):
    model = AutoEncoder()
    model.load_state_dict(torch.load("./reconstruction.pth"))
    img, mask = prep.get_test_image(idx)
-   final_image = img + model(img) * mask
+   out = model(img)
+   prep.show_image(out.squeeze(dim=0))
+   final_image = img + out * mask
    prep.show_image(final_image.squeeze(dim=0))
-   rotated =torchvision.transforms.functional.rotate(final_image, 90)
 
 images = ImageDataset()
-dataloader = DataLoader(images, batch_size=1, shuffle=True)
+dataloader = DataLoader(images, batch_size=5, shuffle=True)
 
-# training_loop(dataloader)
+training_loop(dataloader)
 eval_loop(2)
